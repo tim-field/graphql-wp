9f1a5f437f55c786c98992387441a71ef249d163
JB <jasonbenesch@gmail.com>
2017-01-05 14:02:34 -0800

    add menu item parent field to menu item to create submenus

--- Type/Definition/MenuItem.php
+++ Type/Definition/MenuItem.php
@@ -41,6 +41,20 @@ class MenuItem extends WPObjectType {
                     return $item->title;
                 }
             ],
+            'menu_order' => [
+                'type' => Type::string(),
+                'description' => 'Item order',
+                'resolve' => function($item) {
+                    return $item->menu_order;
+                }
+            ],
+            'menu_item_parent' => [
+                'type' => Type::string(),
+                'description' => 'Item parent',
+                'resolve' => function($item) {
+                    return $item->menu_item_parent;
+                }
+            ],
             'target' => [
                 'type' => Type::string(),
                 'description' => 'Link target',
