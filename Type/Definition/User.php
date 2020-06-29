c46f60eb296357a2319736333466235d10d2b497
Tim Field <field.tim@gmail.com>
2020-06-29 21:44:40 +1200

    More fields for user type

--- Type/Definition/User.php
+++ Type/Definition/User.php
@@ -3,7 +3,7 @@
 namespace Mohiohio\GraphQLWP\Type\Definition;
 
 use GraphQLRelay\Relay;
-
+use GraphQL\Type\Definition\ListOfType;
 use GraphQL\Type\Definition\Type;
 
 class User extends WPObjectType
@@ -17,7 +17,7 @@ class User extends WPObjectType
                 return $user->ID;
             }),
             'ID' => [
-                'type' => Type::nonNull(Type::string()),
+                'type' => Type::nonNull(Type::int()),
                 'description' => 'The ID of the user',
             ],
             'first_name' => [
@@ -31,6 +31,35 @@ class User extends WPObjectType
             'display_name' => [
                 'type' => Type::string(),
                 'description' => 'Users display name'
+            ],
+            'roles' => [
+                'type' => Type::nonNull(new ListOfType(Type::string())),
+                'description' => 'The roles this use is part of'
+            ],
+            'allcaps' => [
+                'type' => Type::nonNull(new ListOfType(Type::string())),
+                'description' => 'All capabilities the user has, including individual and role based.'
+            ],
+            'user_login' => [
+                'type' => Type::nonNull(Type::string()),
+            ],
+            'user_nicename' => [
+                'type' => Type::nonNull(Type::string()),
+            ],
+            'user_email' => [
+                'type' => Type::nonNull(Type::string()),
+            ],
+            'user_url' => [
+                'type' => Type::string(),
+            ],
+            'user_status' => [
+                'type' => Type::string(),
+            ],
+            'is_admin' => [
+                'type' => Type::boolean(),
+                'resolve' => function ($user) {
+                    return in_array('administrator', $user->roles);
+                }
             ]
         ];
     }
